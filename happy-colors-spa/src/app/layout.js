dc816a4939a422035f7cd86b6899d8dfa8c15737
Petya Babukova <p_babukova@abv.bg>
2025-04-22 19:15:11 +0300

    Design phase - start

--- happy-colors-spa/src/app/layout.js
+++ happy-colors-spa/src/app/layout.js
@@ -15,9 +15,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="bg">
+		<html lang="bg" >
 			<body className={`${roboto.variable} antialiased`}>
 				<header className="bg-[#FFF1CD] text-[#003427]">
+					{/* <header className="bg-[#fff] text-[#003427]"> */}
 					<nav>
 						<div className="container mx-auto px-4 flex items-center justify-between py-4 font-sans">
 							{/* Лого отляво */}
@@ -26,14 +27,18 @@ export default function RootLayout({ children }) {
 							</div>
 
 							{/* Основна навигация в средата */}
-							<div className="flex gap-[64px] font-medium text-base">
+							<div className="flex gap-[40px] font-medium text-base">
 								<a href="/" className="nav-link">Начало</a>
-								<a href="/shop" className="nav-link">Магазин</a>
+								<a href="/shop" className="nav-link">Магазин
+									{/* <a>Плетени играчки</a>
+									<a>Плетени кукли</a>
+									<a>Чанти | Раници</a>
+									<a>Шапки | Шалове</a> */}
+								</a>
 								<a href="/aboutus" className="nav-link">За нас</a>
 								<a href="/blog" className="nav-link">Блог</a>
 								<a href="/partners" className="nav-link">За партньори</a>
 								<a href="/contacts" className="nav-link">Контакти</a>
-
 							</div>
 
 							{/* Допълнителни бутони вдясно */}
@@ -45,7 +50,7 @@ export default function RootLayout({ children }) {
 									Задайте въпрос
 								</a> */}
 
-								<a href="/cart" className="text-[#003427] hover:text-[#1A6B41] transition" title="Количка">
+								<a href="/cart" className="text-[#4BD0AE] hover:text-[#1A6B41] transition" title="Количка">
 									<ShoppingCart size={24} />
 								</a>
 							</div>
@@ -53,13 +58,15 @@ export default function RootLayout({ children }) {
 					</nav>
 				</header>
 
-
-
-				<main className="container mx-auto px-4">
+				<main className="container mx-auto px-8">
 					<>
-					{children}
+						{children}
 					</>
 				</main>
+
+				<footer className="bg-[#FFF1CD] text-[#003427] py-4">
+					<p className="container my-0 mx-auto  px-4 flex items-center justify-between font-sans">All rigts reserved 2025</p>
+				</footer>
 			</body>
 		</html>
 	);
