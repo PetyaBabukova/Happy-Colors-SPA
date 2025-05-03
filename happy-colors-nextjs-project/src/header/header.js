b5f93a5556b4ab2a2018c45904a01f956d4ff11f
Petya Babukova <p_babukova@abv.bg>
2025-05-03 19:41:26 +0300

    Header Nav, Desctop styles

--- happy-colors-nextjs-project/src/header/header.js
+++ happy-colors-nextjs-project/src/header/header.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from './header.module.css'
 import Link from 'next/link';
 
-
 export default function Header() {
     return (
         <header>
@@ -14,31 +13,37 @@ export default function Header() {
                 </Link>
 
                 <ul className={styles.mainNavList}>
-                    <li><a href="/">Начало</a></li>
+                    <li><Link href="/">Начало</Link></li>
+
                     <li className={styles.hasSubmenu}>
-                        <Link href="/shop">Магазин</Link>
+                        <Link className={styles.menuItem} href="#">Магазин</Link>
                         <ul className={styles.subNavList}>
-                            <li><Link href="/toys">Животинки и играчки</Link></li>
-                            <li><Link href="/dolls">Кукли с аксесоари</Link></li>
-                            <li><Link href="/scarves-and-hats">Шалове и шапки</Link></li>
-                            <li><Link href="bags">Чанти и раници</Link></li>
+                            <li><Link href="/toys">Животинки</Link></li>
+                            <li><Link href="/dolls">Кукли</Link></li>
+                            <li><Link href="/scarves-and-hats">Шалове</Link></li>
+                            <li><Link href="/bags">Чанти</Link></li>
                         </ul>
                     </li>
+
+
                     <li><Link href="/aboutus">За мен</Link></li>
                     <li><Link href="/blog">Блог</Link></li>
                     <li><Link href="/partners">За партньори</Link></li>
                     <li><Link href="/contacts">Контакти</Link></li>
-                    <li></li>
                 </ul>
+
+
 
                 <form action="" className={styles.searchForm} method="get">
                     <input type="text" placeholder="Търсене" className={styles.searchInput} />
-
-                    <button type="submit" className={styles.searchBtn}><img src='/search_icon_green.svg'></img></button>
+                    <button type="submit" className={styles.searchBtn}>
+                        <img src='/search_icon_green.svg' alt="Търсене" />
+                    </button>
                 </form>
 
-                <Link href="/cart"><img className={styles.basketGreen} src='/basket_green.svg'></img></Link>
-                {/* <Link href="/cart"><img className={styles.basketRed} src='/basket_orange.svg'></img></Link> */}
+                <Link href="/cart">
+                    <img className={styles.basketGreen} src='/basket_green.svg' alt="Количка" />
+                </Link>
             </nav>
         </header>
     )
