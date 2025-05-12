10ee313c9a1d67491b0b2422dc557f271d0c54cf
Petya Babukova <p_babukova@abv.bg>
2025-05-12 20:49:31 +0300

    Login Page Design

--- happy-colors-nextjs-project/src/app/users/login/page.js
+++ happy-colors-nextjs-project/src/app/users/login/page.js
@@ -1,24 +1,18 @@
 import React from 'react'
-import styles from './register.module.css';
+import styles from './login.module.css';
 
 export default function Register() {
     return (
         <div className={styles.registerFormContainer}>
-            <legend>Register</legend>
+            <legend>Login</legend>
             <form className={styles.registerForm} method="POST" action="/users/register">
                 <label for="username">Username</label>
                 <input type="text" name="username" id="username" placeholder='Username'/>
 
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email" placeholder='Email'/>
-
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password" placeholder='Password'/>
 
-                <label for="repeatPassword">Repeat Password</label>
-                <input type="password" name="repeatPassword" id="repeatPassword" placeholder='Repeat Password'/>
-
-                <button >Register</button>
+                <button >Login</button>
             </form>
         </ div>
     )
