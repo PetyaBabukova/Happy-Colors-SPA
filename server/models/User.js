2f09e322afa24472ce788c1821f722d9e6c16608
Petya Babukova <p_babukova@abv.bg>
2025-05-12 20:45:30 +0300

    Register form page design

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
 
@@ -7,27 +8,29 @@ const userSchema = new mongoose.Schema({
         required: [true, "Username is required!"],
         minLength: [3, "The username is too short!"],
         maxLength: [30, "The username is too long!"],
-        match: [/^[\p{L}0-9\s\-\., - – |!?%$&@„“‘’(){}\[\]:;\/\\_=\+\*#^~`]+$/u, "The username contains invalid characters!"],  
+        // match: [/^[\p{L}0-9\s\-\., - – |!?%$&@„“‘’(){}\[\]:;\/\\_=\+\*#^~`]+$/u, "The username contains invalid characters!"],  
     },
 
     password: {
         type: String,
         required: [true, "Password is required!"],
+        minLength: [3, "The password is too short!"]
     },
 
     email: {
         type: String,
         required: [true, "Email is required!"],
-        match: [/^[\p{L}0-9\s\-\., - – |!?%$&@„“‘’(){}\[\]:;\/\\_=\+\*#^~`]+$/u, "The email contains invalid characters!"],  
+        // match: [/^[\p{L}0-9\s\-\., - – |!?%$&@„“‘’(){}\[\]:;\/\\_=\+\*#^~`]+$/u, "The email contains invalid characters!"],  
     },
 
-    dateCreated: String, 
     
-    storageFolder: { 
-        type: String, 
-        default: "userimages",
-    }
 });
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+})
 
 const User = mongoose.model('Product', userSchema);
 export default User;
