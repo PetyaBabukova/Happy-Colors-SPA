4e8b73eccb8c593c0e21494e99de1b8a77e6167a
Petya Babukova <p_babukova@abv.bg>
2026-03-25 21:27:48 +0200

    a dano!

--- server/helpers/sendEmail.js
+++ server/helpers/sendEmail.js
@@ -4,18 +4,28 @@ const EMAIL = process.env.CONTACT_EMAIL;
 const EMAIL_PASSWORD = process.env.CONTACT_EMAIL_PASS;
 const EMAIL_SERVICE = process.env.EMAIL_SERVICE || 'gmail';
 
-const transporter = nodemailer.createTransport({
-  service: EMAIL_SERVICE,
-  auth: {
-    user: EMAIL,
-    pass: EMAIL_PASSWORD,
-  },
-});
+let transporter = null;
 
-export async function sendEmail({ to, subject, text, html }) {
+function getTransporter() {
   if (!EMAIL || !EMAIL_PASSWORD) {
     throw new Error('Липсват CONTACT_EMAIL / CONTACT_EMAIL_PASS в environment variables.');
   }
+
+  if (!transporter) {
+    transporter = nodemailer.createTransport({
+      service: EMAIL_SERVICE,
+      auth: {
+        user: EMAIL,
+        pass: EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  return transporter;
+}
+
+export async function sendEmail({ to, subject, text, html }) {
+  const activeTransporter = getTransporter();
 
   const mailOptions = {
     from: EMAIL,
@@ -25,5 +35,5 @@ export async function sendEmail({ to, subject, text, html }) {
     ...(html ? { html } : {}),
   };
 
-  return transporter.sendMail(mailOptions);
+  return activeTransporter.sendMail(mailOptions);
 }
